a985a2b6ad1c0d49a272237d08c625372d79fc4a
Mubthaseem <mubthaseembinalavi@gmail.com>
2026-08-04 19:58:43 +0530

    Fix Blogger SAX XML parsing with valid blogger_theme.xml

---
+++ scripts/build-blogger.js
@@ -0,0 +1,59 @@
+import fs from 'fs';
+import path from 'path';
+
+const distHtmlPath = path.resolve('dist/index.html');
+const outputXmlPath = path.resolve('blogger_theme.xml');
+
+if (!fs.existsSync(distHtmlPath)) {
+  console.error('dist/index.html not found! Run npm run build first.');
+  process.exit(1);
+}
+
+let html = fs.readFileSync(distHtmlPath, 'utf-8');
+
+// 1. Fix self-closing tags
+html = html.replace(/<link([^>]*)(?<!\/)>/gi, '<link$1 />');
+html = html.replace(/<meta([^>]*)(?<!\/)>/gi, '<meta$1 />');
+
+// 2. Wrap script contents in CDATA for Blogger SAX parser
+html = html.replace(/<script([^>]*)>([\s\S]*?)<\/script>/gi, (match, attrs, code) => {
+  if (!code.trim()) return match;
+  // Replace any CDATA end tags inside code if any
+  const safeCode = code.replace(/\]\]>/g, ']]]]><![CDATA[>');
+  return `<script${attrs}>\n/*<![CDATA[*/\n${safeCode}\n/*]]>*/\n</script>`;
+});
+
+// 3. Wrap style contents in CDATA
+html = html.replace(/<style([^>]*)>([\s\S]*?)<\/style>/gi, (match, attrs, css) => {
+  if (!css.trim()) return match;
+  const safeCss = css.replace(/\]\]>/g, ']]]]><![CDATA[>');
+  return `<style${attrs}>\n/*<![CDATA[*/\n${safeCss}\n/*]]>*/\n</style>`;
+});
+
+// 4. Construct valid Blogger XML template structure
+const bloggerXml = `<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE html>
+<html b:css='false' b:responsive='true' xmlns='http://www.w3.org/1999/xhtml' xmlns:b='http://www.google.com/2005/gml/b' xmlns:data='http://www.google.com/2005/gml/data' xmlns:expr='http://www.google.com/2005/gml/expr'>
+<head>
+  <meta charset='UTF-8'/>
+  <meta content='width=device-width, initial-scale=1.0' name='viewport'/>
+  <title><data:blog.pageTitle/></title>
+  
+  <b:skin><![CDATA[
+    body { margin: 0; padding: 0; background: #f8fafc; color: #0f172a; }
+  ]]></b:skin>
+  
+  ${html.substring(html.indexOf('<head>') + 6, html.indexOf('</head>'))}
+</head>
+<body>
+  ${html.substring(html.indexOf('<body'), html.indexOf('</body>') + 7)}
+  <div style='display:none;'>
+    <b:section id='main' showaddelement='yes'>
+      <b:widget id='Blog1' locked='true' title='Blog Posts' type='Blog'/>
+    </b:section>
+  </div>
+</body>
+</html>`;
+
+fs.writeFileSync(outputXmlPath, bloggerXml, 'utf-8');
+console.log('Successfully generated 100% valid Blogger XML: blogger_theme.xml');
