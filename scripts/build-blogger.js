07476af0db22878e3f7ad0c4b4388d912aa7dec3
Mubthaseem <mubthaseembinalavi@gmail.com>
2026-08-04 20:03:35 +0530

    Update local blogger_theme.xml and blogger_standalone.html with official Blogger CDATA comments

--- scripts/build-blogger.js
+++ scripts/build-blogger.js
@@ -3,6 +3,7 @@ import path from 'path';
 
 const distHtmlPath = path.resolve('dist/index.html');
 const outputXmlPath = path.resolve('blogger_theme.xml');
+const outputHtmlPath = path.resolve('blogger_standalone.html');
 
 if (!fs.existsSync(distHtmlPath)) {
   console.error('dist/index.html not found! Run npm run build first.');
@@ -15,44 +16,44 @@ let html = fs.readFileSync(distHtmlPath, 'utf-8');
 html = html.replace(/\bcrossorigin\b(?!\s*=)/gi, 'crossorigin="anonymous"');
 html = html.replace(/\basync\b(?!\s*=)/gi, 'async="async"');
 html = html.replace(/\bdefer\b(?!\s*=)/gi, 'defer="defer"');
-html = html.replace(/\bdisabled\b(?!\s*=)/gi, 'disabled="disabled"');
-html = html.replace(/\breadonly\b(?!\s*=)/gi, 'readonly="readonly"');
-html = html.replace(/\brequired\b(?!\s*=)/gi, 'required="required"');
 
 // 2. Fix self-closing tags
 html = html.replace(/<link([^>]*)(?<!\/)>/gi, '<link$1 />');
 html = html.replace(/<meta([^>]*)(?<!\/)>/gi, '<meta$1 />');
 
-// 3. Wrap script contents in CDATA for Blogger SAX parser
+// 3. Extract script contents and wrap in Blogger official //<![CDATA[ ... //]]>
 html = html.replace(/<script([^>]*)>([\s\S]*?)<\/script>/gi, (match, attrs, code) => {
   if (!code.trim()) return match;
-  // Fix boolean attributes inside script tag declaration itself
-  let fixedAttrs = attrs
+  
+  // Clean attributes for XML
+  let cleanAttrs = attrs
     .replace(/\bcrossorigin\b(?!\s*=)/gi, 'crossorigin="anonymous"')
     .replace(/\basync\b(?!\s*=)/gi, 'async="async"')
     .replace(/\bdefer\b(?!\s*=)/gi, 'defer="defer"');
   
+  // Ensure type='text/javascript'
+  if (!cleanAttrs.includes('type=')) {
+    cleanAttrs += ' type="text/javascript"';
+  }
+  
+  // Sanitize any CDATA end tags inside code
   const safeCode = code.replace(/\]\]>/g, ']]]]><![CDATA[>');
-  return `<script${fixedAttrs}>\n/*<![CDATA[*/\n${safeCode}\n/*]]>*/\n</script>`;
+  
+  return `<script${cleanAttrs}>\n//<![CDATA[\n${safeCode}\n//]]>\n</script>`;
 });
 
-// 4. Wrap style contents in CDATA
+// 4. Wrap style contents in Blogger official CDATA
 html = html.replace(/<style([^>]*)>([\s\S]*?)<\/style>/gi, (match, attrs, css) => {
   if (!css.trim()) return match;
   const safeCss = css.replace(/\]\]>/g, ']]]]><![CDATA[>');
   return `<style${attrs}>\n/*<![CDATA[*/\n${safeCss}\n/*]]>*/\n</style>`;
 });
 
-// Extract head inner elements
+// Extract head and body
 let headContent = html.substring(html.indexOf('<head>') + 6, html.indexOf('</head>'));
-// Fix any remaining standalone attributes in head
-headContent = headContent.replace(/\bcrossorigin\b(?!\s*=)/gi, 'crossorigin="anonymous"');
-
-// Extract body inner elements
 let bodyContent = html.substring(html.indexOf('<body'), html.indexOf('</body>') + 7);
-bodyContent = bodyContent.replace(/\bcrossorigin\b(?!\s*=)/gi, 'crossorigin="anonymous"');
 
-// 5. Construct valid Blogger XML template structure
+// 5. Construct 100% valid Blogger SAX XML template
 const bloggerXml = `<?xml version="1.0" encoding="UTF-8" ?>
 <!DOCTYPE html>
 <html b:css='false' b:responsive='true' xmlns='http://www.w3.org/1999/xhtml' xmlns:b='http://www.google.com/2005/gml/b' xmlns:data='http://www.google.com/2005/gml/data' xmlns:expr='http://www.google.com/2005/gml/expr'>
@@ -76,4 +77,5 @@ ${bodyContent}
 </html>`;
 
 fs.writeFileSync(outputXmlPath, bloggerXml, 'utf-8');
-console.log('Successfully generated 100% valid Blogger XML with strict attribute values: blogger_theme.xml');
+fs.writeFileSync(outputHtmlPath, bloggerXml, 'utf-8');
+console.log('Successfully updated blogger_theme.xml and blogger_standalone.html with Blogger //<![CDATA[');
